f4b0d2f9b12c193a494e71a9566e5a0c58060704
Ash Lux <ashlux@ashlux-desktop.(none)>
2009-08-18 14:25:41 -0500

    Add test for toString.

---
+++ src/test/java/com/ashlux/games/connectfour/business/deciders/HumanPlayerConsolePromptDeciderTest.java
@@ -0,0 +1,12 @@
+package com.ashlux.games.connectfour.business.deciders;
+
+import org.testng.annotations.BeforeMethod;
+
+public class HumanPlayerConsolePromptDeciderTest
+{
+    @BeforeMethod
+    public void setUp()
+    {
+        // Add your code here
+    }
+}
